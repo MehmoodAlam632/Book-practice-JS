b9a3a33a199aa48b53ec66af6de859711f8f9116
mehmood <mehmoodalam60632@gmail.com>
2022-07-15 09:46:19 +0500

    ES-5 Book practice JS

---
+++ source/app.js
@@ -0,0 +1,71 @@
+// Chapter # 3   Variables for Numbers
+// var weight = 50;
+// weight += 25;
+// console.log('weight :>> ', weight);
+
+// const originalNumber = 53;
+// let newNumber = originalNumber + 17;
+// console.log('newNumber :>> ', newNumber);
+
+// let num1 = 30;
+// let num2 = 60;
+// const sum = num1 + num2;
+// console.log('sum :>> ', sum);
+
+// let roll1 = 234;
+// roll1 = roll1 + 7;
+// console.log('roll1 :>> ', roll1);
+
+// Chapter # 3 Math expressions: Familiar operators
+// const result = 32 % 9
+// console.log('result :>> ', result);
+
+// Chapter # 6 Math expressions: Unfamiliar operators
+// let num = 10;
+// num += 10;
+// console.log("num :>> ", num);
+// let result = 30 + num;
+// console.log("result :>> ", result);
+// result--;
+// console.log('result :>> ', result);
+// result -= 9;
+// console.log('result :>> ', result);
+
+// Chapter # 7 Math expressions: Eliminating ambiguity
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log('resultOfComputation :>> ', resultOfComputation);
+
+// let calculate = (2 * 4) * (4 + 2);
+// console.log('calculate :>> ', calculate);
+
+// Chapter # 8 Concatenating text strings
+// let name = "Mehmood Alam";
+// alert(`Hello ${name}!`);
+
+// Chapter # 9 if statements//////////////
+// let user = prompt("Enter your country Name");
+// if(user === "Pakistan"){
+//     alert("You are pakistani");
+// } else {
+//     alert("Sorry not found");
+// }
+
+// let user = prompt("Enter your country Name");
+// let userInput = "Pakistan";
+// if (user == userInput) {
+//     alert("Correct!");
+// };
+let user = prompt();
+if (user === "pakistan"){
+    alert("Wellcome");
+} else if (user === "Madina"){
+    alert("Wellcome");
+} else if (user === "Makkah"){
+    alert("Wellcome");
+} else if (user === "turkey"){
+    alert("Wellcome");
+} else if (user === "kuwait"){
+    alert("Wellcome");
+} else {
+    alert("Sorry not found");
+};
