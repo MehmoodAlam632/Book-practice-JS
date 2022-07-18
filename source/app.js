d7dbaefb88e389e399b7fc1c38a57c76be4f65c9
mehmood <mehmoodalam60632@gmail.com>
2022-07-18 14:40:16 +0500

    ...

--- source/app.js
+++ source/app.js
@@ -116,3 +116,134 @@
 // Chapter # 15 Array//////////////
 // const cities = ["Karachi","Multan","Islamabad","Lahore"];
 // console.log('cities :>> ', cities[0]);
+
+// Chapter # 15 Array Adding and removing elements//////////////
+// const animals = [];
+// animals[0];
+// animals[3] = "Cat";
+// animals[2] = "Elephant";
+// console.log('animals :>> ', animals);
+// animals[0] = "Lion";
+// console.log('animals[1] :>> ', animals[0]);
+
+// const cars = ["Suzuki", "Honda", "Toyota", "KIA", "Hino"];
+// // console.log('cars :>> ', cars);
+// cars.push("Hyundai");
+// // cars.pop();
+// cars.unshift("BMW");
+// cars.unshift("Ferrari");
+// cars.shift();
+// console.log("cars :>> ", cars);
+
+// Chapter # 17 Array Removing, inserting,and extracting elements//////////////
+// const names = ["Mehmood", "Noureen", "Fareed", "Faraz", "Nousheen", "Siddique"];
+// names.splice(2, 2, "Ali");
+// // names.splice(3, 3);
+// console.log(names);
+
+// let text = "Hello world!";
+// let result = text.slice(0, text.length);
+// console.log(result);
+
+// const names = ["Mehmood", "Noureen", "Fareed", "Faraz", "Nousheen", "Siddique"];
+// let selectNames = names.slice(1, -1);
+// console.log(names);
+// console.log(selectNames);
+
+// Chapter # 18 for loops//////////////
+// let cityToCheck = prompt();
+// const cleanestCities = [
+//   "Islamabad",
+//   "Lahore",
+//   "RawalPindi",
+//   "Makkah",
+//   "Madina",
+// ];
+// if (cityToCheck === "") {
+//   alert("Enter your city name?");
+// } else {
+//   if (cleanestCities.indexOf(cityToCheck) >= 0) {
+//     alert("It's one of the Cleanest City!");
+//   } else {
+//     alert("It's not on the list.");
+//   }
+// }
+// if (cityToCheck === cleanestCities[0]) {
+//   alert("It's one of the Cleanest City!");
+// } else if (cityToCheck === cleanestCities[1]) {
+//   alert("It's Second of the Cleanest City!");
+// }else if(cityToCheck === cleanestCities[2]){
+//   alert("It's Third of the Cleanest City!");
+// } else if(cityToCheck === cleanestCities[3]){
+//   alert("It's Fourth of the Cleanest City!");
+// } else if (cityToCheck === cleanestCities[4]){
+//   alert("It's Fifth of the Cleanest City!");
+// } else {
+//     alert("It's is not on the list.")
+// }
+// console.log(cleanestCities[4]);
+
+//OR//
+//////////////////// Short Way with the help of For loop //////////////////////////
+// for(let i = 0; i < cleanestCities.length; i++){
+//     if(cityToCheck === cleanestCities[i]){
+//         alert("It's one of the Cleanest City!");
+//     } else {
+//         alert("It's is not on the list.");
+//     };
+// };
+
+//OR//
+// const cleanestCities = [
+//   "Islamabad",
+//   "Lahore",
+//   "RawalPindi",
+//   "Makkah",
+//   "Madina",
+// ];
+// let user = prompt("Enter city name");
+// if (!user) {
+//   alert("please type your input");
+// } else {
+//   //   if (cleanestCities.includes(user)) {
+//   //     alert("It's one of the Cleanest City!");
+//   //   } else {
+//   //     alert("It's is not on the list.");
+//   //   }
+//   ///////////////or/////////////////
+// //   if (cleanestCities.filter((e) => e === user).length === 0) {
+// //     alert("It's is not on the list.");
+// //   } else {
+// //     alert("It's one of the Cleanest City!");
+// //   }
+// }
+
+// const cleanestCities = [
+//   "Islamabad",
+//   "Lahore",
+//   "RawalPindi",
+//   "Makkah",
+//   "Madina",
+// ];
+// let cityToCheck = "wddw";
+// let matchFound = false;
+// debugger;
+// for (let i = 0; i <= 4; i++) {
+//   if (cityToCheck === cleanestCities[i]) {
+//     matchFound = true;
+//     alert("It's one of the cleanest cities");
+//     break
+//   }
+// }
+// if (!matchFound) {
+//   alert("It's not on the list");
+// }
+
+
+const cleanestCities = [
+  "Islamabad",
+  "Lahore",
+  "RawalPindi",
+  "Makkah",
+  "Madina",
+];
